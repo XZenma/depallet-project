e0c69ecdabc0acd193422e3175f78aeb4e401ad0
XZenma <154389234+XZenma@users.noreply.github.com>
2026-04-18 21:08:07 +0700

    chore: create admin auth, stall auth, and Table controller

---
+++ app/Http/Controllers/Auth/AdminAuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminAuthController extends Controller
+{
+    public function showLoginForm() {
+
+    }
+
+    public function login(Request $request) {
+
+    }
+
+    public function logout() {
+
+    }
+}
